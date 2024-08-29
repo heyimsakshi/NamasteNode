837cc7cf218e092803b891f8a0944c70c44a7cc1
Sakshi Agrawal <shakshimnnit@gmail.com>
2024-08-30 00:10:17 +0530

    chapter 6  libuv done async.js

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 //  require("./xyz.js")// this line will be executed first the  3 onwards line will be executed (include one module to another module)
-const {calculateSum,multiply} = require("./calculate2")
+//require("node:fs")
+// const fs=require("node:process")
+// const {calculateSum,multiply} = require("./calculate2")
 
-var name="Namaste Node js "
-console.log(name)
-var a=30;
-var b=20;
- calculateSum(a,b);
- multiply(a,b);
+// var name="Namaste Node js "
+// console.log(name, fs.argv);
+// var a=30;
+// var b=20;
+//  calculateSum(a,b);
+//  multiply(a,b);
  //let x=1;
 //  console.log(x)
 // console.log(name);
@@ -16,3 +18,29 @@ var b=20;
 // console.log(global);
 // console.log(this);
 // console.log(globalThis);s
+
+// class Test {
+//     a;
+//     b;
+//     constructor(){
+//         this.i =undefined;
+//         this.j =undefined;
+//         this.fun = ()=>{
+//             console.log("from fun funtion, it is-- ");
+//         }
+
+        
+//     }
+//      fun2(){
+//         console.log("fun222");
+//         this.a=5;
+//         console.log("a ",this.a," b ",this.b)
+//     }
+// }
+
+// const obj = new Test(2,4);
+
+// obj.fun();
+// obj.fun2();
+// console.log(obj.i);
+console.log(global.console);
